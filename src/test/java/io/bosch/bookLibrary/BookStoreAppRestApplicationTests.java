309e511ec9cba965c5ba322c55cd9fdfeb1c3234
FIXED-TERM Boev Boyan (IOB/PDL-BG3) <bob3sf@saz.bosch-si.com>
2022-10-19 11:30:52 +0300

    Spring Boot init

---
+++ src/test/java/io/bosch/bookLibrary/BookStoreAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bosch.bookLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreAppRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
